f4929db5830504ff356a88323f69c84b097fbf44
pritamvidhate <30730433+pritamvidhate@users.noreply.github.com>
2022-02-27 18:37:38 +0530

    Updates

---
+++ precache-manifest.af1d1f4591a3bf5a1fa2641c327f1e64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9e05f41e5daa381ddcf",
+    "url": "/pritamvidhate.github.io/Cryptocurrency-List/static/css/main.f6071297.chunk.css"
+  },
+  {
+    "revision": "c9e05f41e5daa381ddcf",
+    "url": "/pritamvidhate.github.io/Cryptocurrency-List/static/js/main.c9e05f41.chunk.js"
+  },
+  {
+    "revision": "d08479498de0efe7b9da",
+    "url": "/pritamvidhate.github.io/Cryptocurrency-List/static/js/1.d0847949.chunk.js"
+  },
+  {
+    "revision": "752804e2d84446943df0",
+    "url": "/pritamvidhate.github.io/Cryptocurrency-List/static/js/runtime~main.752804e2.js"
+  },
+  {
+    "revision": "3e44d81e9f7956b66df6a24e74f826a2",
+    "url": "/pritamvidhate.github.io/Cryptocurrency-List/index.html"
+  }
+];
